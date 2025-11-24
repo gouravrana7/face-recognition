e8b930af368c843d6f8b3be70bafc572f3d93a47
Gourav Rana <gourav.rana70000@gmail.com>
2025-11-24 22:20:51 +0530

    test

--- faceRecognition.py
+++ faceRecognition.py
@@ -1,34 +1,50 @@
-import cv2, numpy, os
-haar_file = ('haarcascade_frontalface_default.xml')
+import os
+import cv2
+import numpy as np
+
+haar_file = 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(haar_file)
 datasets = 'dataset'
+width, height = 130, 100
+
+if not hasattr(cv2, "face"):
+    raise SystemExit("OpenCV was built without the face module. Install opencv-contrib-python and try again.")
+
 print('Training...')
-(images, labels, names, id) = ([], [], {}, 0)
+images, labels, names = [], [], {}
 
-for (subdirs, dirs, files) in os.walk(datasets):
-    for subdir in dirs:
-        names[id] = subdir
-        subjectpath = os.path.join(datasets, subdir)
-        for filename in os.listdir(subjectpath):
-            path = subjectpath + '/' + 'D:\college\face recognition\face recognition\dataset'
-            label = id
-            images.append(cv2.imread(path, 0))
-            labels.append(int(label))
-        id +=1
+for person_id, subdir in enumerate(sorted(os.listdir(datasets))):
+    subjectpath = os.path.join(datasets, subdir)
+    if not os.path.isdir(subjectpath):
+        continue
+    names[person_id] = subdir
+    for filename in os.listdir(subjectpath):
+        path = os.path.join(subjectpath, filename)
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        if img is None:
+            continue
+        img = cv2.resize(img, (width, height))
+        images.append(img)
+        labels.append(int(person_id))
 
-(images, labels) = [numpy.array(lis) for lis in [images, labels]]
-print(images, labels)                   
-(width, height) = (130, 100)
+if not images:
+    raise SystemExit("No training images found in dataset. Run main.py to create some first.")
+
+images = np.array(images)
+labels = np.array(labels)
 model = cv2.face.LBPHFaceRecognizer_create()
 #model =  cv2.face.FisherFaceRecognizer_create()
 
-model.train(images,labels)
+model.train(images, labels)
 
 webcam = cv2.VideoCapture(0)
 cnt=0
 
 while True:
-    (_, im) = webcam.read()
+    ret, im = webcam.read()
+    if not ret or im is None:
+        print("Failed to capture frame; exiting.")
+        break
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
